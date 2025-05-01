fb2a22dd2e37df73ad3c304ccd6cb8e657b21003
Alamgeer_Khan <ncity243@gmail.com>
2025-05-01 10:24:32 +0500

    size changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <h2 className='text-5xl sm:text-6xl md:text-[72px] inline-block max-w-3xl font-semibold shadow-lg shadow-gray-800 pt-20'>Unlock the Door to Your Dream Home</h2>
         <div className='space-x-6 mt-20'>
             <a href="#Projects" className='border border-white px-10 py-3 rounded sm:px-5 '>Projects</a>
-            <a href="#Contact" className='bg-blue-500 px-10 py-3 rounded sm:px-5 '>Contact Us</a>
+            <a href="#Contact" className='bg-blue-500 px-10 py-3 rounded sm:hidden '>Contact Us</a>
         </div>
       </div>
     </div>
