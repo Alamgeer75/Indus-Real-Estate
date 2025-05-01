d1337e278cc7c111efa62eb43d012e234c066fb0
Alamgeer Khan <140369661+Alamgeer75@users.noreply.github.com>
2025-05-01 09:35:55 +0500

    Update assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -16,7 +16,7 @@ import project_img_6 from './project_img_6.jpg'
 import profile_img_1 from './profile_img_1.png'
 import profile_img_2 from './profile_img_2.png'
 import profile_img_3 from './profile_img_3.png'
-import indus1 from './indus1.svg'
+import indus1 from './Indus1.svg'
 import project from './project.jpg'
 
 export const assets = {
